798181dacba8645570eb7ffd334887e87b12c811
Utama Developer <utamadeveloper@gmail.com>
2022-11-02 14:06:08 +0800

    perbandingan

--- actions/perbandingan/simpan.php
+++ actions/perbandingan/simpan.php
@@ -20,21 +20,43 @@ $in_values = [];
 
 // i = baris
 // j = kolom
+
+// Perhitungan lambda max
+$lambda_max = (float) 0;
 for ($i=1; $i <= 5; $i++) { 
   $in_total = $_POST["totalk".$i];
+  $in_evn = $_POST["evnk".$i];
+  $lambda_max += (float) $in_total * (float) $in_evn;
+}
+
+for ($i=1; $i <= 5; $i++) { 
+  $in_total = $_POST["totalk".$i];
+  $in_evn = $_POST["evnk".$i];
   $in_k = [];
   
+  // Generate SQL values K1,K2,K3,K4,K5
   for ($j=1; $j <= 5; $j++) {
     $in_kolom = $_POST["k".$j."k".$i];
-
     array_push($in_k, $in_kolom);
   } 
 
+  // Perhitungan CI
+  $jumlah_kriteria = 5;
+  $in_ci = (float) ($lambda_max-$jumlah_kriteria)/($jumlah_kriteria-1);
+  
+  // Perhitungan CR
+  $random_index = (float) 1.22;
+  $in_cr = (float) $in_ci / $random_index;
+
+  // Generate SQL values insert
   array_push($in_values, "
     (
       $i,
       ".implode(", ", $in_k).",
-      $in_total
+      $in_total,
+      $in_evn,
+      $in_ci,
+      $in_cr
     )
   ");
 }
@@ -43,12 +65,15 @@ $q_perbandingan = "
   INSERT INTO perbandingan
   (
     idperbandingan,
-    K1,
-    K2,
-    K3,
-    K4,
-    K5,
-    total
+    k1,
+    k2,
+    k3,
+    k4,
+    k5,
+    total,
+    evn,
+    ci,
+    cr
   )
   VALUES
   ".implode(", ", $in_values)."
--- pages/credit/perbandingan.php
+++ pages/credit/perbandingan.php
@@ -33,91 +33,91 @@
           <tr>
             <td class="text-start">Kemampuan</td>
             <td class="text-center">
-              <input id="k1k1" name="k1k1" value="<?=$d_detail? $d_detail[0]['K1'] : '1'?>" type="text" class="number form-control" readonly>
+              <input id="k1k1" name="k1k1" value="<?=$d_detail? $d_detail[0]['k1'] : '1'?>" type="text" class="number form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k1k2" name="k1k2" value="<?=$d_detail? $d_detail[1]['K1'] : ''?>" type="text" class="number form-control" maxlength="1" required>
+              <input id="k1k2" name="k1k2" value="<?=$d_detail? $d_detail[1]['k1'] : ''?>" type="text" class="number form-control" maxlength="1" required>
             </td>
             <td class="text-center">
-              <input id="k1k3" name="k1k3" value="<?=$d_detail? $d_detail[2]['K1'] : ''?>" type="text" class="number form-control" maxlength="1" required>
+              <input id="k1k3" name="k1k3" value="<?=$d_detail? $d_detail[2]['k1'] : ''?>" type="text" class="number form-control" maxlength="1" required>
             </td>
             <td class="text-center">
-              <input id="k1k4" name="k1k4" value="<?=$d_detail? $d_detail[3]['K1'] : ''?>" type="text" class="number form-control" maxlength="1" required>
+              <input id="k1k4" name="k1k4" value="<?=$d_detail? $d_detail[3]['k1'] : ''?>" type="text" class="number form-control" maxlength="1" required>
             </td>
             <td class="text-center">
-              <input id="k1k5" name="k1k5" value="<?=$d_detail? $d_detail[4]['K1'] : ''?>" type="text" class="number form-control" maxlength="1" required>
+              <input id="k1k5" name="k1k5" value="<?=$d_detail? $d_detail[4]['k1'] : ''?>" type="text" class="number form-control" maxlength="1" required>
             </td>
           </tr>
           <tr>
             <td class="text-start">Nilai Jaminan</td>
             <td class="text-center">
-              <input id="k2k1" name="k2k1" value="<?=$d_detail? $d_detail[0]['K2'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k2k1" name="k2k1" value="<?=$d_detail? $d_detail[0]['k2'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k2k2" name="k2k2" value="<?=$d_detail? $d_detail[1]['K2'] : '1'?>" type="text" class="number form-control" readonly>
+              <input id="k2k2" name="k2k2" value="<?=$d_detail? $d_detail[1]['k2'] : '1'?>" type="text" class="number form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k2k3" name="k2k3" value="<?=$d_detail? $d_detail[2]['K2'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
+              <input id="k2k3" name="k2k3" value="<?=$d_detail? $d_detail[2]['k2'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
             </td>
             <td class="text-center">
-              <input id="k2k4" name="k2k4" value="<?=$d_detail? $d_detail[3]['K2'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
+              <input id="k2k4" name="k2k4" value="<?=$d_detail? $d_detail[3]['k2'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
             </td>
             <td class="text-center">
-              <input id="k2k5" name="k2k5" value="<?=$d_detail? $d_detail[4]['K2'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
+              <input id="k2k5" name="k2k5" value="<?=$d_detail? $d_detail[4]['k2'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
             </td>
           </tr>
           <tr>
             <td class="text-start">Pinjaman</td>
             <td class="text-center">
-              <input id="k3k1" name="k3k1" value="<?=$d_detail? $d_detail[0]['K3'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k3k1" name="k3k1" value="<?=$d_detail? $d_detail[0]['k3'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k3k2" name="k3k2" value="<?=$d_detail? $d_detail[1]['K3'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k3k2" name="k3k2" value="<?=$d_detail? $d_detail[1]['k3'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center"> 
-              <input id="k3k3" name="k3k3" value="<?=$d_detail? $d_detail[2]['K3'] : '1'?>" type="text" class="number form-control" readonly>
+              <input id="k3k3" name="k3k3" value="<?=$d_detail? $d_detail[2]['k3'] : '1'?>" type="text" class="number form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k3k4" name="k3k4" value="<?=$d_detail? $d_detail[3]['K3'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
+              <input id="k3k4" name="k3k4" value="<?=$d_detail? $d_detail[3]['k3'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
             </td>
             <td class="text-center">
-              <input id="k3k5" name="k3k5" value="<?=$d_detail? $d_detail[4]['K3'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
+              <input id="k3k5" name="k3k5" value="<?=$d_detail? $d_detail[4]['k3'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
             </td>
           </tr>
           <tr>
             <td class="text-start">Karakter</td>
             <td class="text-center">
-              <input id="k4k1" name="k4k1" value="<?=$d_detail? $d_detail[0]['K4'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k4k1" name="k4k1" value="<?=$d_detail? $d_detail[0]['k4'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k4k2" name="k4k2" value="<?=$d_detail? $d_detail[1]['K4'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k4k2" name="k4k2" value="<?=$d_detail? $d_detail[1]['k4'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k4k3" name="k4k3" value="<?=$d_detail? $d_detail[2]['K4'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k4k3" name="k4k3" value="<?=$d_detail? $d_detail[2]['k4'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k4k4" name="k4k4" value="<?=$d_detail? $d_detail[3]['K4'] : '1'?>" type="text" class="number form-control" readonly>
+              <input id="k4k4" name="k4k4" value="<?=$d_detail? $d_detail[3]['k4'] : '1'?>" type="text" class="number form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k4k5" name="k4k5" value="<?=$d_detail? $d_detail[4]['K4'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
+              <input id="k4k5" name="k4k5" value="<?=$d_detail? $d_detail[4]['k4'] : ''?>" type="text" class="number form-control"  maxlength="1" required>
             </td>
           </tr>
           <tr>
             <td class="text-start">Jangka Waktu</td>
             <td class="text-center">
-              <input id="k5k1" name="k5k1" value="<?=$d_detail? $d_detail[0]['K5'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k5k1" name="k5k1" value="<?=$d_detail? $d_detail[0]['k5'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k5k2" name="k5k2" value="<?=$d_detail? $d_detail[1]['K5'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k5k2" name="k5k2" value="<?=$d_detail? $d_detail[1]['k5'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k5k3" name="k5k3" value="<?=$d_detail? $d_detail[2]['K5'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k5k3" name="k5k3" value="<?=$d_detail? $d_detail[2]['k5'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k5k4" name="k5k4" value="<?=$d_detail? $d_detail[3]['K5'] : ''?>" type="text" class="comma form-control" readonly>
+              <input id="k5k4" name="k5k4" value="<?=$d_detail? $d_detail[3]['k5'] : ''?>" type="text" class="comma form-control" readonly>
             </td>
             <td class="text-center">
-              <input id="k5k5" name="k5k5" value="<?=$d_detail? $d_detail[4]['K5'] : '1'?>" type="text" class="number form-control" readonly>
+              <input id="k5k5" name="k5k5" value="<?=$d_detail? $d_detail[4]['k5'] : '1'?>" type="text" class="number form-control" readonly>
             </td>
           </tr>
           <tr>
@@ -137,6 +137,90 @@
             <th class="text-center">
               <input id="totalk5" name="totalk5" value="<?=$d_detail? $d_detail[4]['total'] : ''?>" type="text" class="comma form-control" readonly>
             </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+
+    <!-- Prioritas -->
+    <div class="table-responsive bg-white mt-4" style="display: none;">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th class="text-center" colspan="8">Nilai rata-rata</th>
+          </tr>
+          <tr>
+            <th class="text-center">X</th>
+            <th class="text-center">K1</th>
+            <th class="text-center">K2</th>
+            <th class="text-center">K3</th>
+            <th class="text-center">K4</th>
+            <th class="text-center">K5</th>
+            <th class="text-center">Total (Nilai rata-rata)</th>
+            <th class="text-center">EVN</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="text-start">K1</td>
+            <td class="text-center" id="pk1k1"></td>
+            <td class="text-center" id="pk1k2"></td>
+            <td class="text-center" id="pk1k3"></td>
+            <td class="text-center" id="pk1k4"></td>
+            <td class="text-center" id="pk1k5"></td>
+            <td class="text-center" id="totalpk1"></td>
+            <td class="text-center">
+              <input id="evnk1" name="evnk1" value="<?=$d_detail? $d_detail[0]['evn'] : ''?>" type="text" class="comma form-control" readonly>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-start">K2</td>
+            <td class="text-center" id="pk2k1"></td>
+            <td class="text-center" id="pk2k2"></td>
+            <td class="text-center" id="pk2k3"></td>
+            <td class="text-center" id="pk2k4"></td>
+            <td class="text-center" id="pk2k5"></td>
+            <td class="text-center" id="totalpk2"></td>
+            <td class="text-center">
+              <input id="evnk2" name="evnk2" value="<?=$d_detail? $d_detail[1]['evn'] : ''?>" type="text" class="comma form-control" readonly>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-start">K3</td>
+            <td class="text-center" id="pk3k1"></td>
+            <td class="text-center" id="pk3k2"></td>
+            <td class="text-center" id="pk3k3"></td>
+            <td class="text-center" id="pk3k4"></td>
+            <td class="text-center" id="pk3k5"></td>
+            <td class="text-center" id="totalpk3"></td>
+            <td class="text-center">
+              <input id="evnk3" name="evnk3" value="<?=$d_detail? $d_detail[2]['evn'] : ''?>" type="text" class="comma form-control" readonly>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-start">K4</td>
+            <td class="text-center" id="pk4k1"></td>
+            <td class="text-center" id="pk4k2"></td>
+            <td class="text-center" id="pk4k3"></td>
+            <td class="text-center" id="pk4k4"></td>
+            <td class="text-center" id="pk4k5"></td>
+            <td class="text-center" id="totalpk4"></td>
+            <td class="text-center">
+              <input id="evnk4" name="evnk4" value="<?=$d_detail? $d_detail[3]['evn'] : ''?>" type="text" class="comma form-control" readonly>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-start">K5</td>
+            <td class="text-center" id="pk5k1"></td>
+            <td class="text-center" id="pk5k2"></td>
+            <td class="text-center" id="pk5k3"></td>
+            <td class="text-center" id="pk5k4"></td>
+            <td class="text-center" id="pk5k5"></td>
+            <td class="text-center" id="totalpk5"></td>
+            <td class="text-center">
+              <input id="evnk5" name="evnk5" value="<?=$d_detail? $d_detail[4]['evn'] : ''?>" type="text" class="comma form-control" readonly>
+            </td>
           </tr>
         </tbody>
       </table>
